422edfc5f5aaac309d14046c69a8c00cf6633a88
kjschelling <kjschelling@gmail.com>
2020-07-13 16:29:55 -0700

    npm installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cocktail from './pages/Cocktail/Cocktail';
 import Cocktails from './pages/Cocktails/Cocktails';
 import {Switch, Route, Link} from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound';
-import InternalServerError from './pages/InternalServerError/InternalServerError';
+import InternalServerError from './pages/InteralServerError/InteralServerError';
 import Offline from './pages/Offline/Offline';
 
 import './App.css';
@@ -13,12 +13,14 @@ function App() {
     <div className="App">
       <header className="App-header">
        <nav>
-          <Link to='/'>Local Quarantine Bartender!</Link>
+          <Link style= {{textDecoration: 'none'}}to='/'>Local Quarantine Bartender!</Link>
         </nav>
       </header>
       <main>
         <Switch>
           <Route exact path="/"/>
+          <Route />
+          <Route />
         </Switch>
       </main>
 
