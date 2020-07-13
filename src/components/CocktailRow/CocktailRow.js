eecf72711fcd5b7592f95ede02a0f72eb776d8c7
kjschelling <kjschelling@gmail.com>
2020-07-13 16:15:40 -0700

    created cocktail row card

--- src/components/CocktailRow/CocktailRow.js
+++ src/components/CocktailRow/CocktailRow.js
@@ -2,7 +2,21 @@ const { Component } = require("react");
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const MonsterRow = () => {
+const MonsterRow = ({idDrink, strDrink, strDrinkThumb}) => {
+    return (
+        <div className="col-md-3">
+          <div className="card mb-4 shadow-sm">
+            <img className="card-img-top" src={strDrinkThumb} alt='Cocktail Pic' />
+            <div className="card-body">
+              <h5 className="card-title">
+                <Link to={`/cocktails/${idDrink}`}>{strDrink}</Link>
+              </h5>
+              <h6 className="card-subtitle mb-2 text-muted">{strDrink}</h6>
+            </div>
+          </div>
+        </div>
+      );
+
 }
 
 export default MonsterRow;
